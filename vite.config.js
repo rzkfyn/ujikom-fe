bc87e2d30d0a45a6f3801b87a761edb01c52b43e
rzkfyn <rzkfyn@gmail.com>
2023-02-24 08:27:48 +0700

    fix: fix unresolveable module alias

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': resolve('src')
+      '@': '/src'
     },
     extensions: [
       '.vue', '.js', '.css'
